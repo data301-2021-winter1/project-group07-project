6b98334a01165f131ccbb26d4a8ae9fbe9c5d9ea
ZUNYI LIN <>
2021-10-27 06:21:42 -0700

    Added project functions.

---
+++ notebooks/project_functions2.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+
+def unprocessed(csv_file):
+    df = pd.read_csv(csv_file)
+    return df
+
+
+
+def load_and_process(csv_file):
+    df = pd.read_csv(csv_file)
+    df
+    df1 = (df.drop(columns = ['usd pledged', 'currency', 'goal', 'pledged'], inplace = False)
+           .rename(columns = {'state':'status'}, inplace = False)
+    return df1
