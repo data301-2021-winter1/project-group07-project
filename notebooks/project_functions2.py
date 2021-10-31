bfd52d76761d1dc7d35d5dac5e6c7663f5b53b89
ZUNYI LIN <>
2021-10-30 18:49:06 -0700

    Task 1

--- notebooks/project_functions2.py
+++ notebooks/project_functions2.py
@@ -12,5 +12,5 @@ def load_and_process(csv_file):
     df = pd.read_csv(csv_file)
     df
     df1 = (df.drop(columns = ['usd pledged', 'currency', 'goal', 'pledged'], inplace = False)
-           .rename(columns = {'state':'status'}, inplace = False)
+           .rename(columns = {'state':'status'}, inplace = False))
     return df1
