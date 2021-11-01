49e0a684890a0d8ef20437b73b8361d0ffb7b427
alexsolll <alexsolo150601@gmail.com>
2021-11-01 04:00:26 -0700

    completed task 1, working on the rest of the assignment

---
+++ notebooks/project_functions1.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+import seaborn as sns
+
+def unprocessed('../data/raw/Gamesales.csv')
+    df = pd.read_csv('../data/raw/Gamesales.csv')
+    return df
